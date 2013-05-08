30dfce4cc3037a06ba24c80672490f71eeabcc35
Daniel Ramsay <dr@circlesquares.co.uk>
2013-05-08 01:08:40 +0100

    split out server

---
+++ FRPServer/event.py
@@ -0,0 +1,37 @@
+
+import logging
+
+from user import User
+
+class EventProcessor(object):
+	def __init__(self, server):
+		self.places = server.places
+		self.users = server.users
+		self.event_pub = server.event_pub
+
+	def process(self, evt, replysock):
+		cmd = evt.cmd.lower()
+		if not hasattr(self, cmd):
+			replysock.send('UNKNOWN')
+			return
+		func = getattr(self, cmd)
+		func(evt, replysock)
+
+	def say(self, evt, replysock):
+		self.event_pub.send("{o.location} [{o.user}] {text}".format(o = evt, text = evt.kw['text']) )
+		replysock.send("OK")
+
+	def enter(self, evt, replysock):
+		logging.info("Got new location for %s: %s", evt.user, evt.location)
+		try:
+			if evt.kw.get('old_location'):
+				self.places[evt.kw['old_location']].users.discard(evt.user)
+		except ValueError: pass
+		self.places[evt.location].users.update([evt.user])
+		self.event_pub.send("{o.location} [{o.user}] arrives.".format(o=evt))
+		replysock.send("OK")
+
+	def announce(self, evt, replysock):
+		logging.info("Generating user record: %s", evt.user)
+		self.users[evt.user] = User.generate(evt.user)
+		replysock.send("OK")
---
+++ FRPServer/request.py
@@ -0,0 +1,64 @@
+
+import logging
+
+from fight import Fight
+
+class RequestProcessor(object):
+	def __init__(self, server):
+		self.places = server.places
+		self.users = server.users
+		self.event_pub = server.event_pub
+		pass
+
+	def process(self, req, replysock):
+		verb = req.req[1:]
+		logging.info("Received verb: %s", verb)
+		if not hasattr(self, verb):
+			logging.info("Unknown command: %s", verb)
+			replysock.send('Unknown Command')
+			return
+		func = getattr(self, verb)
+		logging.debug("Got function: %s", func)
+		try:
+			func(req, replysock)
+		except Exception,v:
+			logging.error("Error: %s", str(v))
+			replysock.send(verb + ' ERR')
+		
+	def look(self, req, replysock):
+		loc = self.places[req.location]
+		reply = loc.describe(req.user)
+		replysock.send("/look " + reply)
+	
+	def loot(self, req, replysock):
+		loc = self.places[req.location]
+		for dead in loc.dead:
+			found = dead.search()
+			if found is not None:
+				replysock.send("/loot You found {0} {1}.".format(found[0], found[1]))
+				break
+		else:
+			replysock.send("/loot You found nothing.")
+				
+	def search(self, req, replysock):
+		loc = self.places[req.location]
+		found = loc.search()
+		if found is None:
+			replysock.send("/search You found nothing.")
+		else:
+			replysock.send("/search You found {0} {1}.".format(found[0], found[1]))
+
+	def attack(self, req, replysock):
+		# assume we're attacking the first enemy
+		loc = self.places[req.location]
+		user = self.users[req.user]
+		if len(loc.enemies) == 0:
+			replysock.send('/attack No enemies to attack')
+		else:
+			enemy = loc.enemies[0]
+			fight = Fight(user, enemy, self.event_pub, ['player-' + user.name])
+			winner = fight.fight()
+			if winner != enemy:
+				loc.enemies.remove(enemy)
+				loc.dead.append(enemy)
+			replysock.send('/attack {0} wins'.format(winner.name))
