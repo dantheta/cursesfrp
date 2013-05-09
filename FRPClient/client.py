183591974f7dc684d788c871a79af11423b19148
Daniel Ramsay <dr@circlesquares.co.uk>
2013-05-09 21:07:59 +0100

    add client config, macros

--- FRPClient/client.py
+++ FRPClient/client.py
@@ -5,6 +5,8 @@ import curses
 import curses.panel
 import logging
 
+import ConfigParser
+
 from FRPShared.model import Event,Request
 from event import EventSource
 from menu import Menu
@@ -19,6 +21,8 @@ class Client(object):
 		self.eventsrc = EventSource(self.user)
 		self.set_location('location-1')
 		self.buffer = ''
+		self.config = ConfigParser.ConfigParser()
+		logging.info("Loaded config: %s", self.config.read(['client.cfg']))
 		pass
 
 	def set_location(self, loc):
@@ -51,6 +55,7 @@ class Client(object):
 	def setup_inputwin(self):
 		self.inputwin = self.stdscr.subwin(9, self.w, self.logline,0)
 		self.inputpanel = curses.panel.new_panel(self.inputwin)
+		self.inputwin.keypad(True)
 		self.inputwin.scrollok(True)
 		self.inputwin.setscrreg(1,7)
 		self.draw_inputwin()
@@ -68,14 +73,6 @@ class Client(object):
 		self.logwin.addstr(self.logline-1,1,s[:78])
 		self.draw_logwin()
 		self.logwin.refresh()
-
-	def kbhit(self):
-		self.inputwin.nodelay(True)
-		ch = self.inputwin.getch()
-		if ch != -1:
-			self.inputwin.ungetch(ch)
-		self.inputwin.nodelay(False)
-		return ch != -1
 
 	def get_input(self):
 		self.inputwin.scroll()
@@ -109,7 +106,28 @@ class Client(object):
 
 	def process_key(self):
 		ch = self.inputwin.getch()
-		if ch == 10:
+		logging.debug("Got key: %s, %s", type(ch), type(curses.KEY_F8))
+		if ch == curses.KEY_F8:
+			s = self.get_macro('F8')
+			logging.debug("Got macro: %s", s)
+			if s:
+				if s.endswith('\n'):
+					self.buffer = ''
+					self.inputwin.scroll()
+					self.inputwin.addstr(7,1, s[:-1])
+					self.inputwin.refresh()
+					return s[:-1]
+				else:
+					self.inputwin.addstr(7,1+len(self.buffer), s)
+					self.buffer += s
+					logging.debug("Buffer: %s", self.buffer)
+					self.inputwin.refresh()
+		elif ch == curses.KEY_BACKSPACE:
+			self.inputwin.addch(7, len(self.buffer), ' ')
+			self.inputwin.move(7,len(self.buffer))
+			self.buffer = self.buffer[:-1]
+			self.inputwin.refresh()
+		elif ch == 10:
 			s = self.buffer
 			self.inputwin.scroll()
 			self.draw_inputwin()
@@ -118,10 +136,19 @@ class Client(object):
 		elif ch == 9:
 			# get options for command so far
 			self.get_options()
+		elif ch > 256:
+			logging.debug("Ignoring key: %s", ch)
 		else:
 			self.inputwin.addch(7, 1+len(self.buffer), ch)
 			self.inputwin.refresh()
 			self.buffer += chr(ch)
+
+	def get_macro(self, key):
+		logging.debug("Looking up macro: %s")
+		try:
+			return self.config.get('macros', key).replace('<CR>','\n')
+		except ConfigParser.NoOptionError:
+			return ''
 
 	def process_input(self, s):
 		if not s.startswith('/'):
