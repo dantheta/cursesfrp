9a0ae55f90b78926bd544d04d16734cb626a1e0c
Daniel Ramsay <dr@circlesquares.co.uk>
2013-05-12 15:49:43 +0100

    line drawing

--- FRPClient/map.py
+++ FRPClient/map.py
@@ -1,36 +1,78 @@
 
+import itertools
+import collections
+
+Location = collections.namedtuple("Location", "pos size exits")
+
 locations = {
-	1: ((1,1), (1,1), [('E',2),('S',3),('W',4)]),
-	2: ((4,1), (1,1), [('W',1)]),
-	3: ((1,4), (1,1), [('N',1)]),
-	4: ((0,1), (1,1), [('E',1)]),
+	1: Location((2,2), (1,1), [('E',2),('S',3),('W',4)]),
+	2: Location((6,2), (3,4), [('W',1)]),
+	3: Location((2,6), (4,1), [('N',1)]),
+	4: Location((0,2), (1,1), [('E',1)]),
 	}
 
 class Grid(object):
+	SCALE = 2
+	TR = u"\u2510"
+	TL = u"\u250c"
+	BL = u"\u2514"
+	BR = u"\u2518"
+	H = u"\u2500"
+	V = u"\u2502"
+	CHARS = [ TL, BL, TR, BR ]
+
 	def __init__(self, size):
 		self.grid = []
 		self.size = size
 		for x in range(size):
 			self.grid.append( [' '] * size)
 
+	def scale(self, x):
+		return x * self.SCALE
+
+	def add_room(self, room):
+		loc = room.pos
+		size = room.size
+		scale = self.scale
+
+		#for x in range((loc[0])*self.SCALE, (loc[0]+sz[0])*self.SCALE):	
+		#	for y in range((loc[1])*self.SCALE, (loc[1]+sz[1])*self.SCALE):	
+		#		self.grid[y][x] = u"\u2588"
+
+		for ch, (x, y) in zip(self.CHARS, itertools.product(
+			(scale(loc[0]), scale(loc[0]+size[0])-1),
+			(scale(loc[1]), scale(loc[1]+size[1])-1),
+			)):
+			self.grid[y][x] = ch
+
+		for x in range(scale(loc[0])+1, scale(loc[0]+size[0])-1):
+			for y in scale(loc[1]), scale(loc[1]+size[1])-1:
+				self.grid[y][x] = self.H
+		for y in range(scale(loc[1])+1, scale(loc[1]+size[1])-1):
+			for x in scale(loc[0]), scale(loc[0]+size[0])-1:
+				self.grid[y][x] = self.V
+			
+
+
+
+	def add_exit(self, _from, _dir, _to):
+		if _dir == 'E':
+			y = ((_from.size[1]/2) + _from.pos[1]) * self.SCALE
+			for x in range((_from.pos[0]+_from.size[0])*self.SCALE, _to.pos[0] * self.SCALE):
+				self.grid[y][x] = u"\u23E4"
+		if _dir == 'S':
+			x =  ((_from.size[0]/2) + _from.pos[0]) * self.SCALE
+			for y in range((_from.pos[1]+_from.size[1])*self.SCALE, _to.pos[1] * self.SCALE):
+				self.grid[y][x] = u"\u007C"
+		
+	def draw_ascii(self):
+		for line in self.grid:
+			print ''.join(line)
+
+grid = Grid(20)
 for room in locations.itervalues():
-	loc = room[0]
-	sz = room[1]
-	for x in range(sz[0]):	
-		for y in range(sz[1]):
-			grid[(loc[1]*2) + y][(loc[0]*2)+x] = u"\u2588"
-	exits = room[2]
+	grid.add_room(room)
+	for (_dir, _to) in room.exits:
+		grid.add_exit(room, _dir, locations[_to])
 
-	for direction, room in exits:
-		if direction == 'E':
-			y = ((sz[1]/2) + loc[1]) * 2
-			for x in range((loc[0]+sz[0])*2, (locations[room][0][0]) * 2):
-				grid[y][x] = u"\u23E4"
-				grid[y][x+1] = u"\u23E4"
-
-#print u"\u23E4"
-#print u"\u2028"
-#print u"\u007C"
-
-for line in grid:
-	print ''.join(line)
+grid.draw_ascii()
